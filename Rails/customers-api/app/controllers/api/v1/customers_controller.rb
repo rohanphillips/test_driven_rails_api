0251c5301ec118dae5ecd1cb1822b9f18bd02bde
rohanphillips <rohan@solutions-insurance.com>
2021-07-01 20:48:03 -0500

    build out customers testing

--- Rails/customers-api/app/controllers/api/v1/customers_controller.rb
+++ Rails/customers-api/app/controllers/api/v1/customers_controller.rb
@@ -3,5 +3,24 @@ class Api::V1::CustomersController < ApplicationController
     @customers = Customer.all
     render json: @customers
   end
+
+  def create
+    @customer = Customer.create(customer_params)
+    if @customer.valid?
+      render json: @customer
+    else
+      render json: @customer.errors, status: 422
+    end
+  end
+
+  def destroy
+    @customer = Customer.find(params[:id])
+    @customer.destroy
+    render status: 204
+  end
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :age, :phone_number)
+  end
 end
 
--- Rails/customers-api/spec/requests/customers_spec.rb
+++ Rails/customers-api/spec/requests/customers_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Customers', type: :request do
   #initialize test data
-  let!(:customers) { FactoryBot.create_list(:customer, 5)}
+  let!(:customers) { create_list(:customer, 5)}
   let!(:customer_id) { customers.first.id }
   #Test suite for GET / customer
   describe 'GET /customers' do
@@ -15,13 +15,43 @@ RSpec.describe 'Customers', type: :request do
       expect(response).to have_http_status(200)
     end
   end
-end
+  #Test suite for POST /customer
+  describe 'POST /customer' do
+    #valid payload
+    let(:valid_customer) {{
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      age: Faker::Number.between(from: 1, to: 100),
+      phone_number: Faker::PhoneNumber.phone_number
+    }}
+    context 'when request is valid' do
+      before { post '/api/v1/customers', params: {customer: valid_customer} }
+      it 'creates a customer' do
+        expect(json["first_name"]).to eq(valid_customer[:first_name])
+      end
+      it 'returns status code 201' do
+        expect(response).to have_http_status(200)
+      end
+    end
 
-# RSpec.describe "Customers", type: :request do
-#   describe "GET /customers" do
-#     it "works! (now write some real specs)" do
-#       get customers_index_path
-#       expect(response).to have_http_status(200)
-#     end
-#   end
-# end
+    context 'when request is invalid' do
+      before { post '/api/v1/customers', params: {customer: {first_name: ""}}}
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+      it 'returns a validation failure message' do
+        expect(json).to include("first_name" => ["can't be blank"])
+        expect(json).to include("last_name" => ["can't be blank"])
+        expect(json).to include("age" => ["can't be blank"])
+        expect(json).to include("phone_number" => ["can't be blank"])
+      end
+    end
+  end
+  #Test suite for DELETE /customer/:id
+  describe 'DELETE /customers/:id' do
+    before { delete "/api/v1/customers/#{customer_id}"}
+    it 'returns status code 204' do
+      expect(response).to have_http_status(204)
+    end
+  end
+end
